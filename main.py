812bb52c8c3ee8d409abb5b4061e7fd4f07fc2db
PhilMurwin <Phil.Murwin@pointbsolutions.com>
2022-03-19 23:28:04 -0500

    Set game name and icon

--- main.py
+++ main.py
@@ -219,6 +219,9 @@ class CRT:
 
 if __name__ == '__main__':
     pygame.init()
+    pygame.display.set_caption('PyInvaders')
+    game_icon = pygame.image.load('graphics/red.png')
+    pygame.display.set_icon(game_icon)
     screen_width = 600
     screen_height = 600
     screen = pygame.display.set_mode((screen_width,screen_height))
